566173bfe78bcee4abca8b8d634e18d7213ef647
tharindu432 <tharinduchaturanga432@gmail.com>
2023-05-06 08:49:31 +0530

    web server

---
+++ index.js
@@ -0,0 +1,19 @@
+const http= require('http');
+const EventEmitter=require('events');
+const fs= require('fs');
+const fsPromises= require('fs').promises;
+
+const logEvents = require('./logEvents');
+
+class Emmiter extends EventEmitter{};
+
+//intialize object
+const myEmitter=new Emmiter();
+
+const PORT=process.env.PORT || 3500;
+
+const server =http.createServer((req,res)=>{
+    console.log(req.url,req.method);
+})
+
+server.listen(PORT,()=>{console.log(`server is running on port ${PORT}`)});
--- logEvents.js
+++ logEvents.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 const logEvents = async(message)=>{
 const dateTime= `${format(new Date(),'yyyyMMdd\tHH:mm:ss')}`;
-const logItem=`${dateTime}\t${uuid()}\t${message}`;
+const logItem=`${dateTime}\t${uuid()}\t${message}\n`;
 //console.log(logItem);
 try{
     await fsPromises.appendFile('./reply.txt',logItem)
